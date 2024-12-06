46cd0ebc11456dcdf7269012672d544b9fd77fc7
tsingbx <tsingbx@gmail.com>
2024-12-06 17:16:24 +0800

    modify cjson_autogen_link.go

--- cjson_autogen_link.go
+++ cjson_autogen_link.go
@@ -1,3 +1,3 @@
 package cjson
 
-const LLGoPackage string = "link: -L/usr/local/Cellar/cjson/1.7.18/lib -lcjson libcjson_utils;"
+const LLGoPackage string = "link: -L/usr/local/Cellar/cjson/1.7.18/lib -lcjson -lcjson_utils;"
